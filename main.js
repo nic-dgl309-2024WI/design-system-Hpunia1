f7241273da9e6761d46c00e9a35446d8d32d0764
Hpunia1 <himanshupunia1994@gmail.com>
2024-04-08 11:42:48 -0700

    Change in css and js

---
+++ main.js
@@ -0,0 +1,47 @@
+let currentPage = 1;
+const projectsPerPage = 3;
+const totalProjects = document.querySelectorAll('.project').length;
+const totalPages = Math.ceil(totalProjects / projectsPerPage);
+
+function toggleHomeMenuClick() {
+    let hamIcon = document.getElementById('hamburger');
+    let closeIcon = document.getElementById('close');
+    let sideNav = document.getElementById('nav');
+    hamIcon.classList.toggle("is-show");
+    closeIcon.classList.toggle("is-show");
+    sideNav.classList.toggle("is-show");
+}
+
+function showProjects(page) {
+    const projects = document.querySelectorAll('.project');
+    projects.forEach((project, index) => {
+        const start = (page - 1) * projectsPerPage;
+        const end = page * projectsPerPage;
+        if (index >= start && index < end) {
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+    });
+}
+
+function previousPage() {
+    let currentElement = document.getElementById('container_02');
+    let pastElement = document.getElementById(`container_01`);
+
+    pastElement.style.display = "none";
+    currentElement.style.display = "flex";
+
+}
+
+function nextPage () {
+    let currentElement = document.getElementById('container_01');
+    let pastElement = document.getElementById(`container_02`);
+
+    pastElement.style.display = "none";
+    currentElement.style.display = "flex";
+
+  }
+
+// Initialize the first page
+showProjects(currentPage);
